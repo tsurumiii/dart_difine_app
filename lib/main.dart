ef04ff06f789e04c5eb223d3be597601ed5eca46
tsurumiii <tsurumi0302@gmail.com>
2022-03-29 23:49:43 +0900

    add app icon

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    const flavor = String.fromEnvironment('FLAVOR');
+    print(flavor);
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
